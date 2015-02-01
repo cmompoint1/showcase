5ea2381cc8d8404f45d7042765d08a1971a50a10
Claude Mompoint <cmompoint@lat-cmompoint.local>
2015-02-01 11:58:03 -0500

    Adding functionality to take a user's name in query string

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,23 @@
 class WelcomeController < ApplicationController
   def index
+  	@name = params[:name]
+  	if @name
+  		session[:name] = @name
+  	end
+
+  	if params[:name]
+  	else
+  		@name = session[:name]
+  	end
   end
 
   def about
+  	@name = session[:name]
   end
-
+  def blog
+  	@name = session[:name]
+  end
   def contact
+  	@name = session[:name]
   end
 end
